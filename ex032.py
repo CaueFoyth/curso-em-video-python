9ea9c232ad365944b6680b799b5e2a810efb52fd
CaueFoyth <foythcaue@gmail.com>
2023-04-18 17:34:32 -0300

    Exercicios

--- ex032.py
+++ ex032.py
@@ -1,5 +1,8 @@
+print("-=-" * 20)
 ano = int(input("Informe um ano para saber se é bisexto: "))
+print("-=-" * 20)
 if ano%4 == 0:
     print("É um ano bisexto!")
 else: 
     print("Não é um ano bisexto!")
+print("-=-" * 20)
